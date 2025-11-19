2e6bdfb81040da8d9e52227ff9a22ab63e90b333
vexuroooxy <valdiazgahari91@gmail.com>
2025-11-19 15:51:41 +0700

    beres

---
+++ script.js
@@ -0,0 +1,11 @@
+function bg_black() {
+  document.getElementById("card").style.backgroundColor = "#212121";
+  document.getElementById("card").style.color = "white";
+  document.body.style.backgroundColor = "#0f0f0f";
+}
+
+function bg_white() {
+  document.getElementById("card").style.backgroundColor = "white";
+  document.getElementById("card").style.color = "black";
+  document.body.style.backgroundColor = "white";
+}
